80f8f88c8a90191ca2b9f15cea5a20746856fc78
Fernando Duarte Villavicencio <al222010329@gmail.com>
2021-12-28 15:48:44 -0600

    Mejoramos la barra de navegacion

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -33,7 +33,23 @@
             <span class="nav-link-text ms-1">Categorias</span>
           </a>
         </li>
+        <li class="nav-item">
+            <a class="nav-link text-white " href="/clientes">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                <i class="material-icons opacity-10">face</i>
+              </div>
+              <span class="nav-link-text ms-1">Clientes</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white " href="/ventas">
+              <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
 
+                <i class="material-icons opacity-10">shop</i>
+              </div>
+              <span class="nav-link-text ms-1">Ventas</span>
+            </a>
+          </li>
       </ul>
     </div>
 
